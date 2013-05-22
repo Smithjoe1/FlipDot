21949345503e6d92a93d4cd53f87f7e9350c7901
Smithjoe1 <Duncan.murray@sjnet.com.au>
2013-05-22 20:10:12 +1000

    Create Observer.java

---
+++ Observer.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Duncan
+ * Date: 9/02/13
+ * Time: 7:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Observer{
+    public void update(char keyCode);
+
+}
