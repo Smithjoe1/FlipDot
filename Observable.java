54e64cd5809e9f1a7f20e5c4d77819b876a833c0
Smithjoe1 <Duncan.murray@sjnet.com.au>
2013-05-22 20:09:56 +1000

    Create Observable.java

---
+++ Observable.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Duncan
+ * Date: 9/02/13
+ * Time: 7:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Observabe{
+    public void notifyObservers(char KeyCode);
+}
